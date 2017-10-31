9559bb2e997e808d53ad891b2e2217a8823abd63
cba1067950 <cba1067950@yahoo.com>
2017-10-31 14:50:45 -0700

    Create JJTestOuput.cpp

---
+++ test/JJTestOuput.cpp
@@ -0,0 +1,57 @@
+#include "JJDisplayOutput.h"
+#include "JJallTest.h"
+
+//Lanes
+int JJallTest::testMaxLanes(){
+    return 0;
+}
+int JJallTest::testMinLanes(){
+    return 0;
+}
+int JJallTest::testLanePosition(){
+    return 0;
+}
+int JJallTest::testTurnArrow(){
+    return 0;
+}
+int JJallTest::testLaneCenter(){
+    return 0;
+}
+
+//Screen
+int JJallTest::testScreenResMax(){
+    return 0;
+}
+int JJallTest::testScreenResMin(){
+    return 0;
+}
+
+//Cars
+int JJallTest::testCarPositionMax(){
+    return 0;
+}
+int JJallTest::testCarPositionMin(){
+    return 0;
+}
+int JJallTest::testCarAngleMax(){
+    return 0;
+}
+int JJallTest::testCarAngleMin(){
+    return 0;
+}
+int JJallTest::testCarAngleNeg(){
+    return 0;
+}
+
+//Lights
+int JJallTest::testLightPosMin(){
+    return 0;
+}
+int JJallTest::testLightPosMax(){
+    return 0;
+}
+int JJallTest::testLightCycleBounds(){
+    return 0;
+}
+
+
